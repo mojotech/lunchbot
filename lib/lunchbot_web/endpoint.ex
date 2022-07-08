6cf682e30e71adff3b3950afbd3df44a72e35c80
Priscila Trevino <priscilatrevino1@gmail.com>
2022-07-08 13:18:00 -0600

    feat: Add torch admin interface

--- lib/lunchbot_web/endpoint.ex
+++ lib/lunchbot_web/endpoint.ex
@@ -22,6 +22,13 @@ defmodule LunchbotWeb.Endpoint do
     gzip: false,
     only: ~w(assets fonts images favicon.ico robots.txt)
 
+  plug Plug.Static,
+    at: "/torch",
+    from: {:torch, "priv/static"},
+    gzip: true,
+    cache_control_for_etags: "public, max-age=86400",
+    headers: [{"access-control-allow-origin", "*"}]
+
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
   if code_reloading? do

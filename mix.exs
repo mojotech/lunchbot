df19a7db7313f6953ee8ca1e5ba6de8978327f17
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2022-07-12 15:26:02 -0600

    chore: Bump phoenix version

--- mix.exs
+++ mix.exs
@@ -34,7 +34,7 @@ defmodule Lunchbot.MixProject do
   defp deps do
     [
       {:bcrypt_elixir, "~> 3.0"},
-      {:phoenix, "~> 1.6.10"},
+      {:phoenix, "~> 1.6.11"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
       {:postgrex, ">= 0.0.0"},

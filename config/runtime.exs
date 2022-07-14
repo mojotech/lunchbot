09c4dab14b5ee2ef00014ed8716a8a2e9bec06de
Joe Alba <joe@joealba.com>
2022-07-14 10:55:10 -0400

    Remove runtime references to Mix in application
    Mix is not available in production releases, so these references to Mix
    prevented deploy to fly.io
    
    The Dotenv.load was moved to config/runtime, as that seems like a more
    appropriate spot -- it _is_ setting up configuration, and it's happening
    at runtime.  :)
    
    References to Mix in the router should be okay, as the router is
    evaluated during compilation, not runtime.

--- config/runtime.exs
+++ config/runtime.exs
@@ -16,6 +16,11 @@ import Config
 #
 # Alternatively, you can use `mix phx.gen.release` to generate a `bin/server`
 # script that automatically sets the env var above.
+unless config_env() == :prod do
+  Dotenv.load()
+  Mix.Task.run("loadconfig")
+end
+
 if System.get_env("PHX_SERVER") do
   config :lunchbot, LunchbotWeb.Endpoint, server: true
 end

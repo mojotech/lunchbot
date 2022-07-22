215feb46584f0bf3a8ccdc4ae80b535d299e122e
RVRX <colemanning@yandex.com>
2022-07-22 15:09:37 -0400

    feat: method to fetch order/item/option total

--- lib/lunchbot/lunchbot_data/order_item.ex
+++ lib/lunchbot/lunchbot_data/order_item.ex
@@ -16,4 +16,13 @@ defmodule Lunchbot.LunchbotData.OrderItem do
     |> cast(attrs, [:order_id, :item_id])
     |> validate_required([:order_id, :item_id])
   end
+
+  def get_total(%Lunchbot.LunchbotData.OrderItem{} = order_item) do
+    total_options_price =
+      Enum.map(order_item.order_item_options, &Lunchbot.LunchbotData.OrderItemOptions.get_total/1)
+      |> Enum.sum()
+
+    # the total price of an OrderItem is the price of it's Options + the Item itself
+    total_options_price + order_item.item.price
+  end
 end
--- lib/lunchbot/lunchbot_data/order_item_options.ex
+++ lib/lunchbot/lunchbot_data/order_item_options.ex
@@ -16,4 +16,13 @@ defmodule Lunchbot.LunchbotData.OrderItemOptions do
     |> cast(attrs, [:order_item_id, :option_id])
     |> validate_required([:order_item_id, :option_id])
   end
+
+  def get_total(%Lunchbot.LunchbotData.OrderItemOptions{} = order_item_option) do
+    # the total price of an OrderItemOption is the price the option + included extras
+    if order_item_option.option.extras do
+      order_item_option.option.extra_price + order_item_option.option.price
+    else
+      order_item_option.option.price
+    end
+  end
 end

c7845d341370c49c972cb4bc7e1d7eab08dde156
RVRX <colemanning@yandex.com>
2022-07-27 17:05:38 -0400

    feat: pull list_order's order into component

--- lib/lunchbot_web/live/previous_orders_live.ex
+++ lib/lunchbot_web/live/previous_orders_live.ex
@@ -24,23 +24,7 @@ defmodule LunchbotWeb.PreviousOrdersLive do
     <%= for an_order <- @user.orders do %>
       <li>ORDER # <%= an_order.id %> || $<%= Order.get_total(an_order) %>
         <ul>
-          <li>From the <%= an_order.menu.name %> Menu</li>
-          <li>Date Ordered: <%= an_order.inserted_at %></li>
-          <li>Items in order:
-            <ul>
-              <%= for an_order_item <- an_order.order_items do %>
-                  <li><%= an_order_item.item.name %>
-                    <%= if !Enum.empty?(an_order_item.order_item_options) do %>
-                      (w/ options:
-                      <%= for an_order_item_option <- an_order_item.order_item_options do %>
-                        <%= an_order_item_option.option.name %>
-                      <% end %>
-                      )
-                    <% end %>
-                  </li>
-              <% end %>
-            </ul>
-          </li>
+          <LunchbotWeb.OrderComponent.display_order order={an_order} />
         </ul>
       </li>
     <% end %>

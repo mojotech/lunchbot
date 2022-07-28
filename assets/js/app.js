a7dd64ee4de7fc6c6c8711cb360a34c83f891017
Korry Tunnicliff <korry.tunnicliff@colorado.edu>
2022-07-27 22:48:09 -0600

    chore: Modal hook and css

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,14 +20,52 @@ import "../css/app.css"
 //
 
 // Include phoenix_html to handle method=PUT/DELETE in forms and buttons.
-import "phoenix_html"
+import 'phoenix_html'
 // Establish Phoenix Socket and LiveView configuration.
-import {Socket} from "phoenix"
-import {LiveSocket} from "phoenix_live_view"
+import {Socket} from 'phoenix'
+import {LiveSocket} from 'phoenix_live_view'
 import topbar from "../vendor/topbar"
 
-let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
+// Define hooks
+const Hooks = {}
+Hooks.ScrollLock = {
+  mounted() {
+    this.lockScroll()
+  },
+  destroyed() {
+    this.unlockScroll()
+  },
+  lockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Add right padding to the body so the page doesn't shift when we disable scrolling
+    const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth
+    document.body.style.paddingRight = `${scrollbarWidth}px`
+    // Save the scroll position
+    this.scrollPosition = window.pageYOffset || document.body.scrollTop
+    // Add classes to body to fix its position
+    document.body.classList.add('fix-position')
+    // Add negative top position in order for body to stay in place
+    document.body.style.top = `-${this.scrollPosition}px`
+  },
+  unlockScroll() {
+    // From https://github.com/excid3/tailwindcss-stimulus-components/blob/master/src/modal.js
+    // Remove tweaks for scrollbar
+    document.body.style.paddingRight = null
+    // Remove classes from body to unfix position
+    document.body.classList.remove('fix-position')
+    // Restore the scroll position of the body before it got locked
+    document.documentElement.scrollTop = this.scrollPosition
+    // Remove the negative top inline style from body
+    document.body.style.top = null
+  }
+}
+let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content");
+let liveSocket = new LiveSocket("/live", Socket, {
+  params: {
+    _csrf_token: csrfToken
+  },
+  hooks: Hooks
+});
 
 // Show progress bar on live navigation and form submits
 topbar.config({barColors: {0: "#29d"}, shadowColor: "rgba(0, 0, 0, .3)"})
